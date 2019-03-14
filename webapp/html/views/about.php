4ccd0f4fcf2d65c0328cfc42c78be2039e497bd0
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2019-03-14 11:03:22 -0400

    Restructurer les fichiers d'exemples dans leurs pages Php respectives

---
+++ webapp/html/views/about.php
@@ -0,0 +1,135 @@
+
+    <!--about-->
+    <!--about-one-->
+    <section class="about-inner py-lg-4 py-md-3 py-sm-3 py-3">
+      <div class="container py-lg-5 py-md-4 py-sm-4 py-3">
+        <h3 class="title text-center mb-md-4 mb-sm-3 mb-3 mb-2">About Us</h3>
+        <div class="title-wls-text text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Lorem ipsum </p>
+        </div>
+        <div class="row">
+          <div class="col-lg-4 w3layouts-right-side-img">
+            <div class="abut-inner-wls-head">
+              <h4 class="pb-3">What Are We</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta</p>
+              <p class="pt-2">sodales scelerisque commodo. Nam porta Vestibulum nibh urna, euismod ut ornare non</p>
+            </div>
+          </div>
+          <div class="col-lg-4 w3layouts-left-side-img">
+            <img src="images/blog2.jpg" class="img-thumbnail" alt="">
+          </div>
+          <div class="col-lg-4 w3layouts-right-side-img">
+            <div class="abut-inner-wls-head">
+              <h4 class="pb-3">What We do</h4>
+              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta</p>
+              <p class="pt-2">Vestibulum nibh urna, euismod ut ornare non, porta Vestibulum nibh urna</p>
+              <div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--//about-->
+    <!--States-->
+    <section class="stats-count">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-lg-6 col-md-6 col-sm-6 stats">
+          </div>
+          <div class="col-lg-6 col-md-6 col-sm-6 counter-number">
+            <div class="row text-center">
+              <div class="col-lg-6 col-md-6 col-sm-6 number-w3three-info ">
+                <h5>7 </h5>
+                <h6 class="pt-2">Branches</h6>
+              </div>
+              <div class="col-lg-6 col-md-6 col-sm-6 number-w3three-info">
+                <h5>250 +</h5>
+                <h6 class="pt-2">Employees</h6>
+              </div>
+              <div class="col-lg-6 col-md-6 col-sm-6 pt-lg-5 pt-md-4 pt-3 number-w3three-info">
+                <h5>150</h5>
+                <h6 class="pt-2">Projects</h6>
+              </div>
+              <div class="col-lg-6 col-md-6 col-sm-6 pt-lg-5 pt-md-4 pt-3 number-w3three-info">
+                <h5>125</h5>
+                <h6 class="pt-2">Tea</h6>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--//States-->
+    <!-- team -->
+    <section class="team py-lg-4 py-md-3 py-sm-3 py-3" id="team">
+      <div class="container py-lg-5 py-md-4 py-sm-4 py-3">
+        <h3 class="title text-center mb-md-4 mb-sm-3 mb-3 mb-2">Our Team</h3>
+        <div class="title-wls-text text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Lorem ipsum </p>
+        </div>
+        <div class="row">
+          <div class="col-lg-3 col-md-6 col-sm-6 team-row-grid">
+            <div class="team-grid">
+              <div class="team-image mb-3">
+                <img src="images/t1.jpg" alt="" class="img-fluid">
+                <div class="social-icons">
+                  <a href="#"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-linkedin" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
+                </div>
+              </div>
+              <h4>William Jon</h4>
+              <p class="mt-2">Director ,CEO</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 team-row-grid">
+            <div class="team-grid">
+              <div class="team-image mb-3">
+                <img src="images/t2.jpg" alt="" class="img-fluid">
+                <div class="social-icons">
+                  <a href="#"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-linkedin" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
+                </div>
+              </div>
+              <h4>Sam Jacob</h4>
+              <p class="mt-2">Director</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 team-row-grid">
+            <div class="team-grid">
+              <div class="team-image mb-3">
+                <img src="images/t3.jpg" alt="" class="img-fluid">
+                <div class="social-icons">
+                  <a href="#"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-linkedin" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
+                </div>
+              </div>
+              <h4>Liam Max</h4>
+              <p class="mt-2">Manager</p>
+            </div>
+          </div>
+          <div class="col-lg-3 col-md-6 col-sm-6 team-row-grid">
+            <div class="team-grid ">
+              <div class="team-image mb-3">
+                <img src="images/t4.jpg" alt="" class="img-fluid">
+                <div class="social-icons">
+                  <a href="#"><span class="fa fa-facebook" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-linkedin" aria-hidden="true"></span></a>
+                  <a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
+                </div>
+              </div>
+              <h4>William jon</h4>
+              <p class="mt-2">Manager</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+    <!--//team -->
---
+++ webapp/html/views/home.php
@@ -0,0 +1,173 @@
+<!-- banner -->
+<div class="main-banner text-center">
+   <div class="container">
+      <div class="style-banner ">
+         <h4 class="mb-lg-3 mb-2">Practice Makes You Better</h4>
+         <h5>Its Time To Master Your Skills
+         </h5>
+      </div>
+      <div class="two-demo-button mt-lg-5 mt-md-4 mt-3">
+         <p> Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet</p>
+      </div>
+      <div class="view-buttn mt-md-4 mt-sm-4 mt-3">
+         <a href="about.html" class="btn">About Us</a>
+      </div>
+   </div>
+</div>
+</div>
+<!-- //banner -->
+<!-- about -->
+<section class="about py-lg-4 py-md-3 py-sm-3 py-3" id="about">
+<div class="container py-lg-5 py-md-4 py-sm-4 py-3">
+   <div class="row">
+      <div class="col-lg-5 col-md-6 about-imgs-txt">
+         <img src="images/ab1.jpg" alt="news image" class="img-fluid">
+      </div>
+      <div class="col-lg-7 col-md-6 text-right about-two-grids">
+         <div class="wthree-about-txt mb-lg-5 mb-md-4 mb-3">
+            <h5>Start Your Carrier</h5>
+         </div>
+         <div class="about-para-txt">
+            <p>sed do eiusmod tempor incididunt ut Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, eiusmod tempor incididunt ut labore et consectetur adipiscing sed do eiusmod tempor incididunt ut Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, eiusmod tempor incididunt ut labore et consectetur adipiscing</p>
+            <p class="mt-2">sed do eiusmod tempor incididunt ut Lorem ipsum dolor sit amet Lorem ipsum dolor sit amet, eiusmod tempor incididunt ut labore et consectetur adipiscing sed do eiusmod</p>
+         </div>
+         <div class="view-buttn mt-lg-5 mt-md-4 mt-3">
+            <a href="about.html" class="">Read More</a>
+         </div>
+      </div>
+   </div>
+   <div class="row mt-lg-5 mt-md-4 mt-3" >
+      <div class="col-lg-3 col-md-6 col-sm-6 about-abt-grid">
+         <div class="about-oil-w3layouts text-center">
+            <div class="about-icon mb-lg-5 mb-md-4 mb-3">
+               <span class="fa fa-th-large" aria-hidden="true"></span>
+            </div>
+            <div class="about-grid-title">
+               <h4 class="mb-lg-3 mb-2">Experienced Staff </h4>
+               <p>sed do eiusmod tempor incididunt</p>
+            </div>
+         </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 about-abt-grid">
+         <div class="about-oil-w3layouts text-center">
+            <div class="about-icon mb-lg-5 mb-md-4 mb-3">
+               <span class="fa fa-refresh" aria-hidden="true"></span>
+            </div>
+            <div class="about-grid-title">
+               <h4 class="mb-lg-3 mb-2">Job Placement </h4>
+               <p>sed do eiusmod tempor incididunt</p>
+            </div>
+         </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 about-abt-grid">
+         <div class="about-oil-w3layouts text-center">
+            <div class="about-icon mb-lg-5 mb-md-4 mb-3">
+               <span class="fa fa-comment" aria-hidden="true"></span>
+            </div>
+            <div class="about-grid-title">
+               <h4 class="mb-lg-3 mb-2">Online Courses</h4>
+               <p>sed do eiusmod tempor incididunt</p>
+            </div>
+         </div>
+      </div>
+      <div class="col-lg-3 col-md-6 col-sm-6 about-abt-grid">
+         <div class="about-oil-w3layouts text-center">
+            <div class="about-icon mb-lg-5 mb-md-4 mb-3">
+               <span class="fa fa-file-text-o" aria-hidden="true"></span>
+            </div>
+            <div class="about-grid-title">
+               <h4 class="mb-lg-3 mb-2">Digital Library</h4>
+               <p>sed do eiusmod tempor incididunt</p>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+</section>
+<!-- //about  -->
+<!-- service -->
+<section class="service py-lg-4 py-md-3 py-sm-3 py-3" id="service">
+<div class="container-fluid py-lg-5 py-md-4 py-sm-4 py-3">
+   <h3 class="title text-center mb-md-4 mb-sm-3 mb-3 mb-2">Our Service</h3>
+   <div class="title-wls-text text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Lorem ipsum </p>
+   </div>
+   <div class="row">
+      <div class="col-lg-7 service-two-grids">
+         <div class="img-cols-grid row">
+            <div class="col-lg-6 col-md-6 col-sm-6 service-grid-wthree text-center">
+               <div class="about-icon mb-md-4 mb-3">
+                  <span class="fa fa-pencil-square-o" aria-hidden="true"></span>
+
+   </div>
+               <div class="ser-sevice-grid">
+                  <h4 class="pb-3">Experienced Staff</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna</p>
+               </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 service-grid-wthree text-center ">
+               <div class="about-icon mb-md-4 mb-3">
+                  <span class="fa fa-book" aria-hidden="true"></span>
+               </div>
+               <div class="ser-sevice-grid ">
+                  <h4 class="pb-3">Spacious Library</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna</p>
+               </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 mt-lg-5 mt-md-4 mt-3 service-grid-wthree  text-center">
+               <div class="about-icon mb-md-4 mb-3">
+                  <span class="fa fa-print" aria-hidden="true"></span>
+               </div>
+               <div class="ser-sevice-grid">
+                  <h4 class="pb-3">Job Placement</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna</p>
+               </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 mt-lg-5 mt-md-4 mt-3 service-grid-wthree  text-center">
+               <div class="about-icon mb-md-4 mb-3">
+                  <span class="fa fa-inbox" aria-hidden="true"></span>
+               </div>
+               <div class="ser-sevice-grid text-center">
+                  <h4 class="pb-3">Unlimited Courses</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna</p>
+               </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 mt-lg-5 mt-md-4 mt-3 service-grid-wthree text-center">
+               <div class="about-icon mb-md-4 mb-3">
+                  <span class="fa fa fa-file-o" aria-hidden="true"></span>
+               </div>
+               <div class="ser-sevice-grid text-center">
+                  <h4 class="pb-3">Digital Library</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna</p>
+               </div>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6 mt-lg-5 mt-md-4 mt-3 service-grid-wthree text-center">
+               <div class="about-icon mb-md-4 mb-3">
+                  <span class="fa fa-globe" aria-hidden="true"></span>
+               </div>
+               <div class="ser-sevice-grid text-center">
+                  <h4 class="pb-3">Online Courses</h4>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna</p>
+               </div>
+            </div>
+         </div>
+      </div>
+      <div class="color-img-five col-lg-5">
+      </div>
+   </div>
+</div>
+</section>
+<!--//service -->
+<!--information -->
+<section class="info py-lg-4 py-md-3 py-sm-3 py-3 text-center">
+<div class="container py-lg-5 py-md-4 py-sm-4 py-3">
+   <div class="info-matter">
+      <h5>If you need Education solution.We are available for you</h5>
+      <p class="mt-lg-4 mt-3">  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna </p>
+   </div>
+   <div class="view-buttn mt-lg-5 mt-md-4 mt-sm-4 mt-3">
+      <a href="contact.html" class="btn ">Contact Us</a>
+   </div>
+</div>
+</section>
+<!--//information -->
