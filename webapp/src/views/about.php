3e27d4fc8b0c8aa624796eda26653bb4ab38a7fb
Jessy Rodrigue <rodrigue_jes@hotmail.com>
2019-03-20 11:33:38 -0400

    Ajout du support de la traduction dans la page À propos

--- webapp/src/views/about.php
+++ webapp/src/views/about.php
@@ -4,22 +4,20 @@ $titre = 'Connection';
     <section class="about-inner py-lg-4 py-md-3 py-sm-3 py-3">
       <div class="container py-lg-5 py-md-4 py-sm-4 py-3">
         <?php
-        if(isset($_SESSION['username'])){
-          ?> <h4 class="text-center mb-md-4 mb-sm-3 mb-3 mb-2"> <?php
-          echo 'Bienvenue, ' . $_SESSION['username'] ;
-          ?> </h4> <?php
+        if (isset($_SESSION['username'])) {
+            ?> <h4 class="text-center mb-md-4 mb-sm-3 mb-3 mb-2"> <?php
+          echo 'Bienvenue, ' . $_SESSION['username'] ; ?> </h4> <?php
         }
         ?>
-        <h3 class="title text-center mb-md-4 mb-sm-3 mb-3 mb-2">About Us</h3>
+        <h3 class="title text-center mb-md-4 mb-sm-3 mb-3 mb-2"><?php echo localize('About-Title'); ?></h3>
         <div class="title-wls-text text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Lorem ipsum </p>
+          <p><?php echo localize('About-Introduction'); ?></p>
         </div>
         <div class="row">
           <div class="col-lg-4 w3layouts-right-side-img">
             <div class="abut-inner-wls-head">
-              <h4 class="pb-3">What Are We</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta</p>
-              <p class="pt-2">sodales scelerisque commodo. Nam porta Vestibulum nibh urna, euismod ut ornare non</p>
+              <h4 class="pb-3"><?php echo localize('About-WhoAreWe'); ?></h4>
+              <p><?php echo localize('About-WhoAreWe-Text'); ?></p>
             </div>
           </div>
           <div class="col-lg-4 w3layouts-left-side-img">
@@ -27,9 +25,8 @@ $titre = 'Connection';
           </div>
           <div class="col-lg-4 w3layouts-right-side-img">
             <div class="abut-inner-wls-head">
-              <h4 class="pb-3">What We do</h4>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum nibh urna, euismod ut ornare non, volutpat vel tortor. Integer laoreet placerat suscipit. Sed sodales scelerisque commodo. Nam porta</p>
-              <p class="pt-2">Vestibulum nibh urna, euismod ut ornare non, porta Vestibulum nibh urna</p>
+              <h4 class="pb-3"><?php echo localize('About-OurGoals'); ?></h4>
+              <p><?php echo localize('About-OurGoals-Text'); ?></p>
               <div>
               </div>
             </div>
@@ -47,20 +44,12 @@ $titre = 'Connection';
           <div class="col-lg-6 col-md-6 col-sm-6 counter-number">
             <div class="row text-center">
               <div class="col-lg-6 col-md-6 col-sm-6 number-w3three-info ">
-                <h5>7 </h5>
-                <h6 class="pt-2">Branches</h6>
+                <h5><?php echo localize('About-Stats-Certs-Count'); ?></h5>
+                <h6 class="pt-2"><?php echo localize('About-Stats-Certs-Text'); ?></h6>
               </div>
               <div class="col-lg-6 col-md-6 col-sm-6 number-w3three-info">
-                <h5>250 +</h5>
-                <h6 class="pt-2">Employees</h6>
-              </div>
-              <div class="col-lg-6 col-md-6 col-sm-6 pt-lg-5 pt-md-4 pt-3 number-w3three-info">
-                <h5>150</h5>
-                <h6 class="pt-2">Projects</h6>
-              </div>
-              <div class="col-lg-6 col-md-6 col-sm-6 pt-lg-5 pt-md-4 pt-3 number-w3three-info">
-                <h5>125</h5>
-                <h6 class="pt-2">Tea</h6>
+                <h5><?php echo localize('About-Stats-Customers-Count'); ?></h5>
+                <h6 class="pt-2"><?php echo localize('About-Stats-Customers-Text'); ?></h6>
               </div>
             </div>
           </div>
@@ -71,9 +60,9 @@ $titre = 'Connection';
     <!-- team -->
     <section class="team py-lg-4 py-md-3 py-sm-3 py-3" id="team">
       <div class="container py-lg-5 py-md-4 py-sm-4 py-3">
-        <h3 class="title text-center mb-md-4 mb-sm-3 mb-3 mb-2">Our Team</h3>
+        <h3 class="title text-center mb-md-4 mb-sm-3 mb-3 mb-2"><?php echo localize('About-OurTeam'); ?></h3>
         <div class="title-wls-text text-center mb-lg-5 mb-md-4 mb-sm-4 mb-3">
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et Lorem ipsum </p>
+          <p><?php echo localize('About-OurTeam-Text'); ?></p>
         </div>
         <div class="row">
           <div class="col-lg-3 col-md-6 col-sm-6 team-row-grid">
@@ -87,8 +76,8 @@ $titre = 'Connection';
                   <a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
                 </div>
               </div>
-              <h4>William Jon</h4>
-              <p class="mt-2">Director ,CEO</p>
+              <h4>Carl Giguère</h4>
+              <p class="mt-2">Orthothérapeute</p>
             </div>
           </div>
           <div class="col-lg-3 col-md-6 col-sm-6 team-row-grid">
@@ -102,43 +91,13 @@ $titre = 'Connection';
                   <a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
                 </div>
               </div>
-              <h4>Sam Jacob</h4>
-              <p class="mt-2">Director</p>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 col-sm-6 team-row-grid">
-            <div class="team-grid">
-              <div class="team-image mb-3">
-                <img src="images/t3.jpg" alt="" class="img-fluid">
-                <div class="social-icons">
-                  <a href="#"><span class="fa fa-facebook" aria-hidden="true"></span></a>
-                  <a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a>
-                  <a href="#"><span class="fa fa-linkedin" aria-hidden="true"></span></a>
-                  <a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
-                </div>
-              </div>
-              <h4>Liam Max</h4>
-              <p class="mt-2">Manager</p>
-            </div>
-          </div>
-          <div class="col-lg-3 col-md-6 col-sm-6 team-row-grid">
-            <div class="team-grid ">
-              <div class="team-image mb-3">
-                <img src="images/t4.jpg" alt="" class="img-fluid">
-                <div class="social-icons">
-                  <a href="#"><span class="fa fa-facebook" aria-hidden="true"></span></a>
-                  <a href="#"><span class="fa fa-twitter" aria-hidden="true"></span></a>
-                  <a href="#"><span class="fa fa-linkedin" aria-hidden="true"></span></a>
-                  <a href="#"><span class="fa fa-google-plus" aria-hidden="true"></span></a>
-                </div>
-              </div>
-              <h4>William jon</h4>
-              <p class="mt-2">Manager</p>
+              <h4>Mélanie Plante</h4>
+              <p class="mt-2">Orthothérapeute</p>
             </div>
           </div>
         </div>
       </div>
     </section>
-<?php $contenu = ob_get_clean(); 
+<?php $contenu = ob_get_clean();
 $onHomePage = false;
 require 'gabarit.php'; ?>
